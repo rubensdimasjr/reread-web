4e1fe25e8d4af8e78f72edb72ec8458f8d0bea77
Rubens Júnior <rubens.jr107@gmail.com>
2022-11-05 21:54:52 -0300

    create loading element

---
+++ loading.js
@@ -0,0 +1,20 @@
+function showLoading(){
+  const div = document.createElement('div');
+  div.classList.add('loading');
+  
+  const label = document.createElement('label');
+  label.innerHTML = "Carregando...";
+
+  div.appendChild(label);
+
+  document.body.prepend(div);
+
+  setTimeout(() => hideLoading(),2000);
+}
+
+function hideLoading(){
+  const loading = document.getElementsByClassName('loading');
+  if(loading.length){
+    loading[0].remove();
+  }
+}
