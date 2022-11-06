adbb7d0c1dbdc2149711f5333476b52186bea0a0
Rubens Júnior <rubens.jr107@gmail.com>
2022-11-06 19:16:23 -0300

    updated switch of categorys after user quiz response

--- escolha-categoria.js
+++ escolha-categoria.js
@@ -1,6 +1,7 @@
 //VERIFICA PERFIL DO USUÁRIO
 firebase.auth().onAuthStateChanged(user => {
   if(user){
+    findProfile(user);
     findResultQuiz(user);
   }
 }); 
@@ -17,12 +18,50 @@ function findResultQuiz(user){
       }))
 
       addResultToScreen(result);
-    });
+    })
+    .catch(() => {
+      alert("Não foi possivel encontrar o resultado do seu quiz!");
+      window.location.href = "quiz.html";
+    })
+}
+
+function findProfile(user){
+  firebase.firestore()
+    .collection('profile')
+    .where('user.uid', '==',user.uid)
+    .get()
+    .then(snapshot => {
+      const result = snapshot.docs.map(doc => ({
+        ...doc.data(),
+        uid: doc.id
+      }))
+
+      if(result[0].quiz == false){
+        update(result[0]);
+      }
+      
+    })
+    .catch(() => {
+      alert("Não foi possivel encontrar seu perfil!");
+      window.location.href = "index.html";
+    })
+}
+
+function update(user){
+  firebase.firestore()
+  .collection('profile')
+  .doc(user.uid)
+  .update({quiz: true})
+  .catch(() => {
+    alert("Erro ao atualizar perfil");
+  })
 }
 
 function addResultToScreen(result){
   const card = document.querySelectorAll(".card");
-  card.forEach(result => {
-    
+
+  card.forEach((value,key) => {
+    card[key].children[1].innerHTML = result[0].result[key].categoria
+    card[key].href = "cursos.html?categoria="+result[0].result[key].categoria;
   })
 }
