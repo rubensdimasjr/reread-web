ecc6173ae71c48a8026f29aed5031c18d91dbbdd
Rubens Júnior <rubens.jr107@gmail.com>
2022-11-07 15:02:03 -0300

    updated home creating swiper slide

--- home.js
+++ home.js
@@ -1,6 +1,24 @@
 /* ACTIVE LINK */
 const sections = document.querySelectorAll('section[id]');
 
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 10,
+  breakpoints: {
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 50,
+    },
+  },
+});
 
 function scrollActive(){
   const scrollY = window.pageYOffset;
